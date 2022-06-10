2a958fa939f20ee869aba79f81a777cc61afdfa9
gammablaze <gammablaze@gmail.com>
2022-06-10 19:20:30 +0100

    Fixing module and backend code.

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -1,9 +1,9 @@
 locals {
-  http_port = 80
-  any_port = 0
+  http_port    = 80
+  any_port     = 0
   any_protocol = "-1"
   tcp_protocol = "tcp"
-  all_ips = ["0.0.0.0/0"]
+  all_ips      = ["0.0.0.0/0"]
 }
 
 data "aws_vpc" "default" {
@@ -19,9 +19,9 @@ data "terraform_remote_state" "db" {
 
   config = {
     bucket = var.db_remote_state_bucket
-    key = var.db_remote_state_key
+    key    = var.db_remote_state_key
     region = "us-east-2"
-   }
+  }
 }
 
 data "template_file" "user_data" {
@@ -29,24 +29,16 @@ data "template_file" "user_data" {
 
   vars = {
     server_port = var.server_port
-    db_address = data.terraform_remote_state.db.outputs.address
-    db_port = data.terraform_remote_state.db.outputs.port
-  }
-}
-
-# Partial configuration. The other settings (e.g. bucket, region) will be passed
-# in from a file via -backend-config arguments to "terraform init"
-terraform {
-  backend "s3" {
-      key = "stage/services/webserver-cluster/terraform.tfstate"
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
   }
 }
 
 resource "aws_launch_configuration" "example" {
-  image_id = "ami-0c55b159cbfafe1f0"
-  instance_type = var.instance_type
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
-  user_data =  data.template_file.user_data.rendered
+  user_data       = data.template_file.user_data.rendered
 
   # Required when using a launch configuration with an auto scaling group.
   # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
@@ -57,18 +49,18 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size = var.min_size
-  max_size = var.max_size
+  min_size         = var.min_size
+  max_size         = var.max_size
   desired_capacity = 4
 
   tag {
-    key = "Name"
-    value = var.cluster_name
+    key                 = "Name"
+    value               = var.cluster_name
     propagate_at_launch = true
   }
 }
@@ -77,24 +69,24 @@ resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
 
   ingress {
-    from_port = local.any_port
-    to_port = local.any_port
-    protocol = local.tcp_protocol
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.tcp_protocol
     cidr_blocks = local.all_ips
   }
 }
 
 resource "aws_lb" "example" {
-  name = "${var.cluster_name}-alb"
+  name               = "${var.cluster_name}-alb"
   load_balancer_type = "application"
-  subnets = data.aws_subnet_ids.default.ids
-  security_groups = [aws_security_group.alb.id]
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port = local.http_port
-  protocol = "HTTP"
+  port              = local.http_port
+  protocol          = "HTTP"
 
   # By default, return a simple 404 page
   default_action {
@@ -103,40 +95,40 @@ resource "aws_lb_listener" "http" {
     fixed_response {
       content_type = "text/plain"
       message_body = "404: page not found"
-      status_code = 404
+      status_code  = 404
     }
   }
 }
 
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
-  priority = 100
+  priority     = 100
 
   condition {
     path_pattern {
-     values = ["*"] 
+      values = ["*"]
     }
   }
 
   action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.asg.arn
   }
 }
 
 resource "aws_lb_target_group" "asg" {
-  name = "${var.cluster_name}-asg"
-  port = var.server_port
+  name     = "${var.cluster_name}-asg"
+  port     = var.server_port
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 15
-    timeout = 3
-    healthy_threshold = 2
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 }
@@ -146,21 +138,21 @@ resource "aws_security_group" "alb" {
 }
 
 resource "aws_security_group_rule" "allow_http_inbound" {
-  type = "ingress"
+  type              = "ingress"
   security_group_id = aws_security_group.alb.id
 
-  from_port = local.http_port
-  to_port = local.http_port
-  protocol = local.tcp_protocol
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
   cidr_blocks = local.all_ips
 }
 
 resource "aws_security_group_rule" "allow_all_outbound" {
-  type = "egress"
+  type              = "egress"
   security_group_id = aws_security_group.alb.id
 
-  from_port = local.any_port
-  to_port = local.any_port
-  protocol = local.any_protocol
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
   cidr_blocks = local.all_ips
 }
