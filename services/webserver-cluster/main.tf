79b1281f31f64e6f3aee80ceb4886aadd17f83f2
gammablaze <gammablaze@gmail.com>
2022-06-27 21:35:12 +0100

    Fixing user_data.

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -31,6 +31,7 @@ data "template_file" "user_data" {
     server_port = var.server_port
     db_address  = data.terraform_remote_state.db.outputs.address
     db_port     = data.terraform_remote_state.db.outputs.port
+    server_text = var.server_text
   }
 }
 
@@ -38,12 +39,7 @@ resource "aws_launch_configuration" "example" {
   image_id        = var.ami
   instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
-  user_data = templatefile("${path.module}/user-data.sh", {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-    server_text = var.server_text
-  })
+  user_data       = data.template_file.user_data.rendered
 
   # Required when using a launch configuration with an auto scaling group.
   # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
